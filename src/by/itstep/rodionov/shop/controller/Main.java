4e3f2b071689a7f7d550bc58189a838e126e4268
MeHail98 <mehail.conversation@gmil.com>
2022-04-27 19:52:28 +0300

    2 entity added

---
+++ src/by/itstep/rodionov/shop/controller/Main.java
@@ -0,0 +1,10 @@
+package by.itstep.rodionov.shop.controller;
+
+import by.itstep.rodionov.shop.model.entity.Orange;
+
+public class Main {
+    public static void main(String[] args) {
+        Orange orange = new Orange(200, 3000, 4.5);
+
+    }
+}
